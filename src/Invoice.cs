9fc3b158e8ed1f7da00de77019943598783b13f3
Adeel Nasir <adeel.nasir@rillion.com>
2026-02-20 18:35:44 +0100

    add invoice changes

---
+++ src/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace src
+{
+    public class Invoice
+    {
+        public object SupplierId { get; internal set; }
+        public string ErpType { get; internal set; }
+
+        internal bool IsValid()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/InvoiceProcessor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class InvoiceProcessor
+    {
+        public void Process(Invoice invoice)
+        {
+            if (!invoice.IsValid())
+            {
+                Reject("Invalid payload");
+                return;
+            }
+
+            if (!SupplierExists(invoice.SupplierId))
+            {
+                Reject("Supplier does not exist");
+                return;
+            }
+
+            switch (invoice.ErpType)
+            {
+                case "D365":
+                    PostToD365(invoice);
+                    break;
+
+                case "SAP":
+                    PostToSap(invoice);
+                    break;
+
+                default:
+                    Reject("Unsupported ERP");
+                    break;
+            }
+
+            MarkAsPosted(invoice);
+        }
+
+        private bool SupplierExists(object supplierId)
+        {
+            return true;
+        }
+
+        void Reject(string reason) { }
+        bool SupplierExists(string id) => true;
+        void PostToD365(Invoice invoice) { }
+        void PostToSap(Invoice invoice) { }
+        void MarkAsPosted(Invoice invoice) { }
+    }
+
+}
